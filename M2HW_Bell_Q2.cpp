7483327dca7f86258461c3900ddc064526e8ecdb
Jordan <bellj4473@student.faytechcc.edu>
2026-03-02 11:49:38 -0500

    Named M2HW_Q2

---
+++ M2HW_Bell_Q2.cpp
@@ -0,0 +1,46 @@
+// CSC 134
+// M2HW_Q2 - Silver
+// Jordan Bell
+// 2/23/26
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Updated rates
+    const double COST_PER_CUBIC_FOOT = 0.30;
+    const double CHARGE_PER_CUBIC_FOOT = 0.52;
+
+    double length,
+    width,
+    height,
+    volume,
+    cost,
+    charge,
+    profit;
+
+    cout << setprecision(2) << fixed << showpoint;
+
+    cout << "Enter the dimensions of the crate (in feet):\n";
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ";
+    cin >> height;
+
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost;
+
+    cout << "The volume of the crate is ";
+    cout << volume << " cubic feet.\n";
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
+    
+    return 0;
+}
